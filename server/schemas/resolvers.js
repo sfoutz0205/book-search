0151685250f9d2c9792c7a2bf38352eee0fd8d91
sfoutz0205 <68661461+sfoutz0205@users.noreply.github.com>
2021-01-10 19:03:43 -0700

    typeDefs

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,9 +1,7 @@
+const { User, Book } = require('../models');
+
 const resolvers = {
-  Query: {
-    helloWorld: () => {
-      return 'Hello world!';
-    }
-  }
+  
 };
 
 module.exports = resolvers;
--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,8 +1,25 @@
 const { gql } = require('apollo-server-express');
 
 const typeDefs = gql`
-  type Query {
-    helloWorld: String
+  type User {
+    _id: ID
+    username: String
+    email: String
+    bookCount: Int
+    savedBooks: [Book]
+  }
+
+  type Book {
+    bookId: String
+    authors: String
+    description: String
+    image: String
+    link: String
+    title: String
+  }
+  type Auth {
+    token: ID!
+    user: User
   }
 `;
 
