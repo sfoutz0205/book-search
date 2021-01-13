69f824c85586e543480532fafcdbf7e33441e380
sfoutz0205 <68661461+sfoutz0205@users.noreply.github.com>
2021-01-12 17:42:40 -0700

    add mutations

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,7 +1,71 @@
+const { AuthenticationError } = require('apollo-server-express');
 const { User, Book } = require('../models');
+const auth = require('../utils/auth');
 
 const resolvers = {
-  
+  Query: {
+    getMe: async (parent, args, context) => {
+      if (context.user) {
+        const profile = await User.findOne({ _id: context.user._id })
+          .select('-__v -password')
+          .populate('savedBooks')
+
+        return profile;
+      }
+      throw new AuthenticationError('Not logged in')
+    }
+  },
+  Mutation: {
+    addUser: async (parent, args) => {
+      const user = await User.create(args);
+      const token = signToken(user);
+      return { token, user };
+    },
+
+    saveBook: async (parent, { book }, context) => {
+      if (context.user) {
+        const user = await User.findByIdAndUpdate(
+          { _id: context.user._id },
+          { $push: { savedBooks: book }},
+          { new: true }
+        );
+
+        return user;
+      }
+
+      throw new AuthenticationError('Please login to complete this action!');
+    }, 
+
+    removeBook: async (parent, { bookId }, context) => {
+      if (context.user) {
+        const User = await User.findByIdAndUpdate(
+          { _id: context.user._id },
+          {$pull: { savedBooks: { bookId: bookId }}},
+          { new: true }
+        );
+
+        return user;
+      }
+
+      throw new AuthenticationError('Please login to complete this action!');
+    }, 
+
+    login: async (parent, { email, password }) => {
+      const user = await User.findOne({ email });
+        if (!user) {
+          throw new AuthenticationError('Incorrect username');
+      }
+
+      const correctPw = await user.isCorrectPassword(password);
+        if (!correctPw) {
+          throw new AuthenticationError('Incorrect password!');
+      }
+      
+      const token = signToken(user);
+      return { token, user };
+      
+    }
+  }
 };
 
 module.exports = resolvers;
--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -11,15 +11,36 @@ const typeDefs = gql`
 
   type Book {
     bookId: String
-    authors: String
+    authors: [String]
     description: String
     image: String
     link: String
     title: String
   }
+
+  type BookInput {
+    bookId: String
+    authors: [String]
+    description: String
+    title: String
+    image: String
+    link: String
+  }
+
   type Auth {
     token: ID!
     user: User
+  }
+
+  type Query {
+    getMe: User
+  }
+
+  type Mutation {
+    login(email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
+    saveBook(book: BookInput!): User
+    removeBook(bookId: String!): User
   }
 `;
 
